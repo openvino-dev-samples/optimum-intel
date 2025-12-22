0156d090b3ca3d135260af1f125777a64b701e93
ethan <ethan.yang@intel.com>
2025-12-22 06:44:29 -0800

    remove clamp

--- optimum/exporters/openvino/model_patcher.py
+++ optimum/exporters/openvino/model_patcher.py
@@ -3455,56 +3455,6 @@ def _zimage_forward(
     return x[0]  # Return single tensor instead of (x, {})
 
 
-def _zimage_transformerblock_forward(
-    self,
-    x: torch.Tensor,
-    attn_mask: torch.Tensor,
-    freqs_cis: torch.Tensor,
-    adaln_input: Optional[torch.Tensor] = None,
-):
-    if self.modulation:
-        assert adaln_input is not None
-        scale_msa, gate_msa, scale_mlp, gate_mlp = self.adaLN_modulation(adaln_input).unsqueeze(1).chunk(4, dim=2)
-        gate_msa, gate_mlp = gate_msa.tanh(), gate_mlp.tanh()
-        scale_msa, scale_mlp = 1.0 + scale_msa, 1.0 + scale_mlp
-
-        # Attention block
-        attn_out = self.attention(
-            self.attention_norm1(x) * scale_msa,
-            attention_mask=attn_mask,
-            freqs_cis=freqs_cis,
-        )
-        x = x + gate_msa * self.attention_norm2(attn_out)
-
-        # FFN block
-        # Update: clamp the output of feed_forward to avoid overflow in float16
-        x = x + gate_mlp * self.ffn_norm2(
-            torch.clamp(
-                self.feed_forward(
-                    self.ffn_norm1(x) * scale_mlp,
-                ),
-                torch.finfo(torch.float16).min,
-                torch.finfo(torch.float16).max,
-            )
-        )
-    else:
-        # Attention block
-        attn_out = self.attention(
-            self.attention_norm1(x),
-            attention_mask=attn_mask,
-            freqs_cis=freqs_cis,
-        )
-        x = x + self.attention_norm2(attn_out)
-
-        # FFN block
-        x = x + self.ffn_norm2(
-            self.feed_forward(
-                self.ffn_norm1(x),
-            )
-        )
-
-    return x
-
 def _zimage_rope_embedder_precompute_freqs_cis(dim: List[int], end: List[int], theta: float = 256.0):
     with torch.device("cpu"):
         freqs_cis = []
@@ -3744,18 +3694,12 @@ class ZImageTransformerModelPatcher(ModelPatcher):
         )
 
         for layer in self._model.noise_refiner:
-            layer._orig_forward = layer.forward
-            layer.forward = types.MethodType(_zimage_transformerblock_forward, layer)
             layer.attention._orig_processor = layer.attention.processor
             layer.attention.processor = PatchedZSingleStreamAttnProcessor()
         for layer in self._model.context_refiner:
-            layer._orig_forward = layer.forward
-            layer.forward = types.MethodType(_zimage_transformerblock_forward, layer)
             layer.attention._orig_processor = layer.attention.processor
             layer.attention.processor = PatchedZSingleStreamAttnProcessor()
         for layer in self._model.layers:
-            layer._orig_forward = layer.forward
-            layer.forward = types.MethodType(_zimage_transformerblock_forward, layer)
             layer.attention._orig_processor = layer.attention.processor
             layer.attention.processor = PatchedZSingleStreamAttnProcessor()
 
@@ -3777,15 +3721,12 @@ class ZImageTransformerModelPatcher(ModelPatcher):
         self._model.patchify_and_embed = self._orig_patchify_and_embed
         
         for layer in self._model.noise_refiner:
-            layer.forward = layer._orig_forward
             layer.attention.processor = layer.attention._orig_processor
         
         for layer in self._model.context_refiner:
-            layer.forward = layer._orig_forward
             layer.attention.processor = layer.attention._orig_processor
         
         for layer in self._model.layers:
-            layer.forward = layer._orig_forward
             layer.attention.processor = layer.attention._orig_processor
 
 def _minicpmv_resampler_forward(self, image_feature, pos_embed, key_padding_mask):

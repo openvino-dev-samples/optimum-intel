9107f8b15313d96431b7c63f4738e02213f3a9f1
Dark-Hacker-10 <103432983+Dark-Hacker-10@users.noreply.github.com>
2026-04-22 12:49:09 +0530

    [Bug] Add missing OVErnieImagePipeline dummy class
    OVErnieImagePipeline is registered in optimum/intel/__init__.py under both the dummy import structure and the real import structure, but the corresponding dummy fallback class was missing from dummy_openvino_and_diffusers_objects.py.
    
    This causes an ImportError when a user installs optimum-intel without openvino or diffusers and tries to import OVErnieImagePipeline, instead of the expected helpful error message.
    
    Add the dummy class following the same pattern as all other OV pipeline dummy classes.

--- optimum/intel/utils/dummy_openvino_and_diffusers_objects.py
+++ optimum/intel/utils/dummy_openvino_and_diffusers_objects.py
@@ -266,3 +266,14 @@ class OVSanaSprintPipeline(metaclass=DummyObject):
     @classmethod
     def from_pretrained(cls, *args, **kwargs):
         requires_backends(cls, ["openvino", "diffusers"])
+
+
+class OVErnieImagePipeline(metaclass=DummyObject):
+    _backends = ["openvino", "diffusers"]
+
+    def __init__(self, *args, **kwargs):
+        requires_backends(self, ["openvino", "diffusers"])
+
+    @classmethod
+    def from_pretrained(cls, *args, **kwargs):
+        requires_backends(cls, ["openvino", "diffusers"])
